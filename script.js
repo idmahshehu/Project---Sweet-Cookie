f1676ca7afb24a24c2fa43e8eb4f2d5d7eeefa93
idmahshehu <122348949+idmahshehu@users.noreply.github.com>
2023-01-22 16:21:41 +0100

    Topics Bar

--- script.js
+++ script.js
@@ -8,22 +8,55 @@ userpic.addEventListener("click", function () {
   profilemenu.classList.toggle("active");
 });
 
-
 // ----side bar----
-const guideBar = document.getElementById('guide-bar');
+const guideBar = document.getElementById("guide-bar");
 console.log(guideBar);
-const sidebar = document.getElementById('side-bar_p');
+const sidebar = document.getElementById("side-bar_p");
 // const subImg = document.getElementById("sub-img");
-guideBar.addEventListener("click", function(){
-    sidebar.classList.toggle("side-bar_p_active");
-    subImg.classList.toggle("sub-img_active");
-    sidebar.querySelectorAll(".guide-items").forEach((a) => {
-        a.classList.toggle("side-bar_active");
-    });
-    sidebar.querySelectorAll(".guide-items-p").forEach((p) => {
-        p.classList.toggle("guide-items-p_active");
-    })
-
+guideBar.addEventListener("click", function () {
+  sidebar.classList.toggle("side-bar_p_active");
+//   subImg.classList.toggle("sub-img_active");
+  sidebar.querySelectorAll(".guide-items").forEach((a) => {
+    a.classList.toggle("side-bar_active");
+  });
+  sidebar.querySelectorAll(".guide-items-p").forEach((p) => {
+    p.classList.toggle("guide-items-p_active");
+  });
 });
 
 
+// ---Tabs Switch---
+function setupTabs() {
+    document.querySelectorAll(".tabs__button").forEach((button) => {
+      button.addEventListener("click", () => {
+        const switchTopics = button.parentElement;
+        const mainContainer = switchTopics.parentElement;
+        const headerContainer = mainContainer.parentElement;
+        const tabsContainer=headerContainer.parentElement;
+        const tabNumber = button.dataset.forTab;
+        const tabToActivate = tabsContainer.querySelector(
+          `.tabs__content[data-tab="${tabNumber}"]`
+        );
+  
+        tabsContainer.querySelectorAll(".tabs__content").forEach((tab) => {
+          tab.classList.remove("tabs__content--active");
+        });
+  
+        tabToActivate.classList.add("tabs__content--active");
+      });
+    });
+  }
+  document.addEventListener("DOMContentLoaded", () => {
+    setupTabs();
+  
+    document.querySelectorAll("body").forEach(tabsContainer => {
+    tabsContainer.querySelector(".switch_topics .tabs__button").click();
+    //   let click = true;
+
+    //   tabsContainer.querySelectorAll("button").forEach((button) => {
+    //     button.classList.add("tabs__button_color");
+    //   });
+    //   tabsContainer.querySelector("button").classList.add("tabs__button_color");
+      
+    });
+  });
